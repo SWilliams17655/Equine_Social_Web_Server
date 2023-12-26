e6846ea6f4eec16953307816d17bef2447ff0862
Sean Williams <SWilliams17655@hotmail.com>
2023-12-26 09:55:42 -0600

    Creating login page.

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask
+import flask_login
+import werkzeug.security
+import os
+from dotenv import load_dotenv
+from flask import Flask, render_template, request, redirect
+from flask_login import UserMixin, LoginManager, login_required
+from flask_sqlalchemy import SQLAlchemy
+from datetime import date
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World32!'
+def home_page():
+    return render_template('index.html', user_file=None)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
